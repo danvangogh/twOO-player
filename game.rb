c31801520ff71e3550ecd3a22c95b857b0d47a90
danvangogh <djameswhite@yahoo.com>
2018-12-05 04:25:51 +0000

    game finished

--- game.rb
+++ game.rb
@@ -1,7 +1,53 @@
 class Game
   attr_accessor :current_player
-  
-  def initialize(current_player)
-    @current_player = current_player
+
+  def initialize
+    @players = [
+      Player.new("Player 1"),
+      Player.new("Player 2")
+    ]
+    @current_player = @players[0]
   end
+
+  def play
+    while @players[0].score_count > 0 && @players[1].score_count > 0 do
+
+      q = Question.new
+
+      puts "#{@current_player.name} #{q}"
+      player_response = gets.chomp.to_i
+      result = q.check_answer(player_response)
+      check_answer(result)
+    end
+    loser_town
+    puts "----- NEW TURN -----"
+    puts "Good bye!"
+  end
+
+  def check_answer(res)
+    if res == false
+      @current_player.score_count -= 1
+      puts "That's a lost life, pal"
+    else
+      puts "Congrats at not sucking"
+    end
+    puts "P1: #{@players[0].score_count}/3 vs P2: #{@players[1].score_count}/3"
+    player_switch
+    unless @players[0].score_count == 0 || @players[1].score_count == 0
+      puts "----- NEW TURN -----"
+    end
+  end
+
+  def player_switch
+    if @current_player == @players[0]
+      @current_player = @players[1]
+    else
+      @current_player = @players[0]
+    end
+  end
+
+  def loser_town
+    puts "#{@current_player.name} wins with a score of #{@current_player.score_count}/3"
+  end
+
 end
--- question.rb
+++ question.rb
@@ -1,11 +1,14 @@
 class Question
-  attr_accessor :num_1, :num_2, :answer
+  attr_reader :num_1, :num_2, :answer
 
-  def show_question
+  def initialize
     @num_1 = rand(0...10)
     @num_2 = rand(0...10)
     @answer = @num_1 + @num_2
-    puts "What is the total of #{@num_1} and #{@num_2}"
+  end
+
+  def to_s
+    "What is the total of #{@num_1} and #{@num_2}"
   end
 
   def check_answer(useranswer)
